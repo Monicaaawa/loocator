dcf284129218d71874b6fe22a698ad8dab374d40
Shiyu Ye <105144733+Monicaaawa@users.noreply.github.com>
2023-04-22 13:33:46 -0700

    implement map

--- loocator/App.js
+++ loocator/App.js
@@ -1,11 +1,45 @@
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 34.068921,
+    longitude: -118.4451811,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const userLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+    }
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+    console.log(location);
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    })
+  }
+
+  useEffect(() => {
+    userLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MapView
+        style={{height:'100%', width: '100%'}}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        region={mapRegion}
+      />
     </View>
   );
 }
