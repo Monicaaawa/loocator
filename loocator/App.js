c031af1802a3bd60ba963ed5f80846033143c293
Jenna Wang <jenna2004@gmail.com>
2023-04-22 19:01:41 -0700

    start screen

--- loocator/App.js
+++ loocator/App.js
@@ -1,11 +1,49 @@
 import * as React from 'react';
 import MainContainer from './src/MainContainer';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./src/screens/HomeScreen.js"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, View, Text, Image } from 'react-native';
 
+// function SplashScreen({navigation})
+// {
+//   setTimeout(() => {
+//     navigation.navigate('Home');
+//   }, 2000);
+//   return (
+//     <View style={styles.container}>
+//     <Text style={styles.title}> loocator </Text>
+    
+//     <Image
+//         style={styles.duckLogo}
+//         source={require('./src/screens/assets/rubber-duck.png')}/>
+//     </View>
+//   );
+// }
+
+// const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <MainContainer/>
+    <>
+      <MainContainer/>
+      {/* <NavigationContainer>
+        <Stack.Navigator initialRouteName = "SplashScreen" screenOptions={{headerShown: false,}}>
+        <Stack.Screen
+          name = "SplashScreen"
+          component = {SplashScreen}
+        />
+        <Stack.Screen
+          name = "Home"
+          component = {HomeScreen}
+        />
+        <Stack.Screen
+          name = "MainContainer"
+          component = {MainContainer}
+        />
+        </Stack.Navigator>
+      </NavigationContainer> */}
+    </>
   );
 }
 
@@ -16,4 +54,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  title: {
+    fontSize: 30,
+  },
+  start: {
+    fontSize: 20,
+    borderColor: 'black',
+    borderWidth: 2,
+    padding: 8,
+    borderRadius: 20
+  },
+  duckLogo: {
+    width: 150,
+    height: 150,
+    margin: 50,
+  },
+});  
--- loocator/src/MainContainer.js
+++ loocator/src/MainContainer.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 //Screens
@@ -16,52 +16,145 @@ const homeName = 'Home';
 const profileName = 'Profile';
 const settingName = 'Setting';
 
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-export default function MainContainer(){
-    return(
-        <NavigationContainer>
-            <Tab.Navigator 
-                initial RouteName={homeName}
-                screenOptions={
-                    ({route})=>({
-                    tabBarIcon: ({focused, color, size}) => {
-                        let iconName;
-                        let rn = route.name;
+function SplashScreen({navigation})
+{
+  setTimeout(() => {
+    navigation.navigate("MainScreen");
+  }, 2000);
+  return (
+    <View style={styles.container}>
+    <Text style={styles.title}> loocator </Text>
+    
+    <Image
+        style={styles.duckLogo}
+        source={require('./screens/assets/rubber-duck.png')}/>
+    </View>
+  );
+}
 
-                        if (rn == homeName){
-                            iconName = focused ? 'home' : 'home-outline';
-                        } else if (rn == profileName){
-                            iconName = focused ? 'list' : 'list-outline'; //TODO BROOKE: change logo later
-                        } else if (rn == settingName){
-                            iconName = focused ? 'settings' : 'settings-outline';
-                        } 
+function MainScreen() {
+    return (
+        <Tab.Navigator
+            initialRouteName = "Home"
+            screenOptions= {{headerShown: false}}>
+        
+            <Tab.Screen
+                name = "Home"
+                component = {HomeScreen}
+                options = {{ title: 'Home',
+                    tabBarIcon: ({focused, size, color}) => (
+                        <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={color}/>
+                    )
+                }}
+            />
 
-                        return <Ionicons name={iconName} size={size} color={color}/>
-                    },
-                    "tabBarActiveTintColor": 0xA4BFFF,
-                        "tabBarInactiveTintColor": "grey",
-                        "tabBarLabelStyle": {
-                            "paddingBottom": 10,
-                            "fontSize": 10
-                        },
-                        "tabBarStyle": [
-                            {
-                            "display": "flex"
-                            },
-                            null
-                        ],
-                })}
-                >
-                
-                <Tab.Screen name={homeName} component = {HomeScreen}/>
-                <Tab.Screen name={profileName} component = {ProfileScreen}/>
-                <Tab.Screen name={settingName} component = {SettingScreen}/>
-            
-            </Tab.Navigator>
-        </NavigationContainer>
-        // <View>
-        //     <Text>This file is for Nav Bar stuff. Ignorime if not Brooke</Text>
-        // </View>
+            <Tab.Screen
+                name = "Profile"
+                component = {ProfileScreen}
+                options = {{ title: 'Profile',
+                    tabBarIcon: ({focused, size, color}) => (
+                        <Ionicons name={focused ? 'list' : 'list-outline'} size={size} color={color}/>
+                    )
+                }}
+            />  
+
+            <Tab.Screen
+                name = "Setting"
+                component = {SettingScreen}
+                options = {{ title: 'Setting',
+                    tabBarIcon: ({focused, size, color}) => (
+                        <Ionicons name={focused ? 'settings' : 'settings-outline'} size={size} color={color}/>
+                    )
+                }}
+            /> 
+
+        </Tab.Navigator>
     )
 }
+
+function MainContainer(props){
+    return(
+        // <NavigationContainer>
+        //     <Tab.Navigator 
+        //         initial RouteName={HomeScreen}
+        //         screenOptions={
+        //             ({route})=>({
+        //             tabBarIcon: ({focused, color, size}) => {
+        //                 let iconName;
+        //                 let rn = route.name;
+
+        //                 if (rn == homeName){
+        //                     iconName = focused ? 'home' : 'home-outline';
+        //                 } else if (rn == profileName){
+        //                     iconName = focused ? 'list' : 'list-outline'; //TODO BROOKE: change logo later
+        //                 } else if (rn == settingName){
+        //                     iconName = focused ? 'settings' : 'settings-outline';
+        //                 } 
+
+        //                 return <Ionicons name={iconName} size={size} color={color}/>
+        //             },
+        //             "tabBarActiveTintColor": 0xA4BFFF,
+        //                 "tabBarInactiveTintColor": "grey",
+        //                 "tabBarLabelStyle": {
+        //                     "fontSize": 10
+        //                 },
+        //                 "tabBarStyle": [
+        //                     {
+        //                     "display": "flex"
+        //                     },
+        //                     null
+        //                 ],
+        //         })}
+        //         >
+                
+        //         {/* <Tab.Screen name="SplashScreen" component = {SplashScreen}/> */}
+        //         <Tab.Screen name={homeName} component = {HomeScreen}/>
+        //         <Tab.Screen name={profileName} component = {ProfileScreen}/>
+        //         <Tab.Screen name={settingName} component = {SettingScreen}/>
+            
+        //     </Tab.Navigator>
+        // </NavigationContainer>
+
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName = "SplashScreen" screenOptions={{headerShown: false,}}>
+            <Stack.Screen
+                name = "SplashScreen"
+                component = {SplashScreen}
+            />
+            <Stack.Screen
+                name = "MainScreen"
+                component = {MainScreen}
+            />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 30,
+    },
+    start: {
+      fontSize: 20,
+      borderColor: 'black',
+      borderWidth: 2,
+      padding: 8,
+      borderRadius: 20
+    },
+    duckLogo: {
+      width: 150,
+      height: 150,
+      margin: 50,
+    },
+  });  
+
+  export default MainContainer;
