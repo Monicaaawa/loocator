895635dd44dbc9e53a182939bb5d2659f311d6a6
Brooke Jiang <jiangxuan@g.ucla.edu>
2023-04-23 00:03:44 -0700

    backend implementation w firebase

--- loocator/App.js
+++ loocator/App.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import MainContainer from './src/MainContainer';
 import SignInScreen from './src/screens/SignInScreen/SignInScreen';
 
+
+
 export default function App() {
   return (
     // <MainContainer/>
--- loocator/firebase.js
+++ loocator/firebase.js
@@ -1,27 +1,26 @@
-// // Import the functions you need from the SDKs you need
-// import firebase from 'firebase/compat/app';
-// import 'firebase/compat/auth';
-// import 'firebase/compat/firestore';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+import * as firebase from "firebase";
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDQqGEFzqZx3cRwpFrs_zlSfNovqXNQhTE",
-//   authDomain: "loocator-aa951.firebaseapp.com",
-//   projectId: "loocator-aa951",
-//   storageBucket: "loocator-aa951.appspot.com",
-//   messagingSenderId: "805033013198",
-//   appId: "1:805033013198:web:14eb9fc14dabc11db0059f"
-// };
+// TODO: Add SDKs for Firebase rproducts that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Initialize Firebase
-// let app;
-// if (firebase.apps.length === 0) {
-//     app = initializeApp(firebaseConfig);
-// } else {
-//     app = firebase.app()
-// }
-// const auth = firebase.auth()
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQqGEFzqZx3cRwpFrs_zlSfNovqXNQhTE",
+  authDomain: "loocator-aa951.firebaseapp.com",
+  projectId: "loocator-aa951",
+  storageBucket: "loocator-aa951.appspot.com",
+  messagingSenderId: "805033013198",
+  appId: "1:805033013198:web:14eb9fc14dabc11db0059f"
+};
 
-// export { auth };
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
